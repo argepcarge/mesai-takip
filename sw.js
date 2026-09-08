f40eb209dffc5ad3d974615408a51b25c7ecab27
argepcarge <argepcarge@gmail.com>
2026-09-08 08:45:03 +0300

    Upgrade service worker to version 2 with new assets

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
-const CACHE_NAME = 'mesai-pwa-v1';
-const assets = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'mesai-pwa-v2';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './exceljs.min.js',
+  './sablon.xlsx',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(assets))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
